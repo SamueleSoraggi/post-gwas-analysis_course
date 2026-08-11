7948e73b5f60544cb5647f6c7c4b648d9479d123
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-08-11 11:25:32 +0000

    Built site for gh-pages

--- Scripts/start_jupyter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# =============================================================================
-
-CONTAINER="gwas-minimal.sif"
-
-
-apptainer exec "${CONTAINER}" pixi run -m /opt/gwas/pixi.toml jupyter lab --ip=$(hostname) --no-browser --port=$UID
