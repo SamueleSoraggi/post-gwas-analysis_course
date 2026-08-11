9c84a1fc45bcd55596fc2b66d2ee8ef0f14e89a6
SamueleSoraggi <samuelson1990@gmail.com>
2026-08-11 13:20:02 +0200

    updates webpage

--- post-gwas-course.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# =============================================================================
-
-CONTAINER="gwas-minimal.sif"
-
-apptainer exec "${CONTAINER}" pixi run -m /opt/gwas/pixi.toml jupyter lab --ip=$(hostname) --port=$UID --allow-root
